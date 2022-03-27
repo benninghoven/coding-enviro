6bd39e669f709c73f52174b4807b9187b9432a7e
Fishology <devinbenninghoven@gmail.com>
2022-03-27 02:56:20 -0700

    fixing sudo privs

--- src/depedents.sh
+++ src/depedents.sh
@@ -14,7 +14,7 @@ InstallBrew(){
 
 InstallAPT(){
     echo installing packages
-    [[ "$(sudo -l 2>&1 /dev/null)" == *"Sorry"* ]] || echo no sudo rights rip
+    [[ "$(sudo -l 2>&1 /dev/null)" == *"Sorry"* ]] && echo no sudo rights rip || echo you are a sudo chad
     sudo apt-get update && sudo apt-get dist-upgrade -y
     sudo apt-get install build-essential curl file git
 }
